3c1272ba02167b435198a54018a0a37e2ca90586
Zack Michael <zack@squishmedia.com>
2015-09-21 12:46:41 -0700

    initial commit

---
+++ config.js
@@ -0,0 +1,11 @@
+export default {
+	port: 8765,
+	headers: {
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+	},
+	scitran: {
+		baseURL: 'https://scitran.sqm.io/api/',
+		secret: 'a6c1f717eb4b438b80d6baa2866c2dc8',
+	}
+};
